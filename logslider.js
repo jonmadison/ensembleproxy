3c0e37d8cdb2865e201d45ab582d0851737d949e
jon madison <jon@tfftech.com>
2015-02-07 16:06:20 -0800

    logslider should take an argument duh

--- logslider.js
+++ logslider.js
@@ -1,7 +1,7 @@
 module.exports = function(min,max) {
     console.log("loaded logslider: ("+min+","+max+")")
     return {
-        logslider: function() {
+        logslider: function(position) {
             // position will be between 0 and 100
             var minp = 0;
             var maxp = 100;
