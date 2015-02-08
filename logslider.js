d7dc84ca6ad9447e4672fdf2199b1fde2c175b7d
jon madison <jon@tfftech.com>
2015-02-07 16:05:29 -0800

    proper syntax for module derp

--- logslider.js
+++ logslider.js
@@ -1,19 +1,20 @@
 module.exports = function(min,max) {
     console.log("loaded logslider: ("+min+","+max+")")
+    return {
+        logslider: function() {
+            // position will be between 0 and 100
+            var minp = 0;
+            var maxp = 100;
 
-    var logslider = function() {
-    // position will be between 0 and 100
-    var minp = 0;
-    var maxp = 100;
+            // The result should be between 100 an 10000000
+            var minv = Math.log(min);
+            var maxv = Math.log(max);
 
-    // The result should be between 100 an 10000000
-    var minv = Math.log(min);
-    var maxv = Math.log(max);
+            // calculate adjustment factor
+            var scale = (maxv-minv) / (maxp-minp);
 
-    // calculate adjustment factor
-    var scale = (maxv-minv) / (maxp-minp);
-
-    return Math.exp(minv + scale*(position-minp));    
-  }
+            return Math.exp(minv + scale*(position-minp));    
+        }
+    }
 }
 
